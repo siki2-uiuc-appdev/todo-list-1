300982c74e975332f7013f37ba9b66c120538cb6
siki2-uiuc <siki2@illinois.edu>
2022-11-07 22:27:45 +0000

    Tasks db migrated

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  status     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  owner_id   :integer
+#
 class Task < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_07_222157) do
+ActiveRecord::Schema.define(version: 2022_11_07_222451) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "body"
+    t.string "status"
+    t.integer "owner_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
